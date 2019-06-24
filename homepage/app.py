509ee3fcc0ebf2a246b067f7770330ab6fbbea7a
Yudhajit N <28938427+Sh3llcod3@users.noreply.github.com>
2019-06-24 01:00:21 +0100

    More work done on site.

--- homepage/app.py
+++ homepage/app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+# Import required modules
+
 from __future__ import unicode_literals
 
 from flask import Flask, render_template, request
 
 import youtube_dl
 
-
+# Setup our Video class, this will handle the youtube_dl side of things.
 class Video():
+
+    # Initialise the class.
     def __init__(self, video_link):
         self.video_link = video_link
         self.ydl_opts = {
@@ -19,6 +23,7 @@ class Video():
             'outtmpl': './downloads/%(title)s.%(ext)s'
         }
 
+    # Add our download() method to download the video.
     def download(self):
         with youtube_dl.YoutubeDL(self.ydl_opts) as self.ydl:
             self.ydl.download([self.video_link])
@@ -30,12 +35,16 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/', methods=["GET", "POST"])
 def index_page():
     if request.method == "GET":
-        return render_template("./site.html")
+        with open("./templates/site.html", "r") as site:
+            return site.read()
+        #return render_template("./site.html")
 
     if request.method == "POST":
-        dl_request = Video(request.form["videoURL"])
-        dl_request.download()
-        return request.form["videoURL"]
+        # dl_request = Video(request.form["videoURL"])
+        #dl_request.download()
+        # kek = [request.form["videoURL"], request.form["radio_1"], request.form["radio_2"], request.form["attach_thumb"]]
+        print(request.form)
+        return request.form["radio_1"]
 
 
 def main():
