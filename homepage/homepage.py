d8f674b99d61bfe5f60059205232a1757250a5df
Yudhajit N <28938427+Sh3llcod3@users.noreply.github.com>
2019-06-28 01:28:21 +0100

    Reworked file transfer system.

--- homepage/homepage.py
+++ homepage/homepage.py
@@ -4,20 +4,29 @@
 from __future__ import unicode_literals
 
 from atexit import register
-from base64 import b64encode
 from datetime import datetime
-from os import walk
+from os import environ, makedirs, path, walk
 from shutil import make_archive
 from subprocess import call, check_output  # noqa: S404
 from sys import argv, exit
 
 import easyparse
 
-from flask import Flask, jsonify, render_template, request, safe_join, send_from_directory
+from flask import Flask, render_template, request, safe_join, send_from_directory
 
 import youtube_dl
 
 version_string = " * HomePage, v0.1.0\n * Copyright (c) 2019 Sh3llcod3. (MIT License)"
+
+# Get the environment paths
+storage_folder = environ.get("HOMEPAGE_STORAGE")
+downloads_folder = environ.get("HOMEPAGE_DOWNLOADS")
+
+storage_folder = storage_folder if (storage_folder is not None) else "~/.homepage_storage"
+downloads_folder = downloads_folder if (downloads_folder is not None) else "~/.homepage_downloads"
+
+storage_folder = path.expanduser(storage_folder)
+downloads_folder = path.expanduser(downloads_folder)
 
 
 # Setup our Video class, this will handle the youtube_dl side of things.
@@ -35,7 +44,7 @@ class Video():
                 'preferredcodec': f'{self.mime_type}',
                 'preferredquality': '192',
             }],
-            'outtmpl': './downloaded_tracks/%(title)s.%(ext)s'
+            'outtmpl': f'{downloads_folder}/%(title)s.%(ext)s'
         }
         if post_request["attach_thumb"].lower() == "yes":
             self.ydl_opts["writethumbnail"] = True
@@ -52,38 +61,32 @@ class Video():
 
     # Add our send_files() method to handle transfer.
     def send_files(self):
-        path, dirs, files = next(walk("./downloaded_tracks/"))
+        path, dirs, files = next(walk(downloads_folder))
         file_count = len(files)
         self.final_file_name = str()
         self.final_file_location = str()
 
         # The link is invalid
         if file_count == 0:
-            with open("./templates/error_template.html", "r") as error:
-                return error.read()
+            return render_template("error_template.html")
 
         # We have more than one file, so let's zip them up and send them back.
         if file_count > 1:
             self.final_file_name = "tracks_" + str(datetime.now().timestamp()).replace('.', '')
             self.final_file_location = "/tmp/" + self.final_file_name  # noqa: S108
-            make_archive(self.final_file_location, 'zip', "downloaded_tracks")
+            make_archive(self.final_file_location, 'zip', downloads_folder)
             self.final_file_name += ".zip"
             self.final_file_location += ".zip"
             self.mime_type = "application/zip"
+            call(f"mv {self.final_file_location} {storage_folder}/", shell=True)  # noqa: S607, S602
+            call(f"rm {downloads_folder}/*", shell=True)  # noqa: S607, S602
 
         # We only have one track, so let's send the file back.
         else:
-            self.final_file_name = check_output("ls ./downloaded_tracks/",  # noqa: S607
-                                                  shell=True).decode("utf-8").rstrip()  # noqa: S602
-            self.final_file_location = "./downloaded_tracks/" + self.final_file_name
+            self.final_file_name = next(walk(downloads_folder))[2][0]
+            call(f"mv {downloads_folder}/* {storage_folder}/", shell=True)  # noqa: S607, S602
 
-        with open(self.final_file_location, "rb") as file_to_send:
-            file_to_send = b64encode(file_to_send.read()).decode()
-
-        call("cp ./downloaded_tracks/* ./downloads/", shell=True)  # noqa: S607, S602
-        call("rm ./downloaded_tracks/*", shell=True)  # noqa: S607, S602
-        call(f"rm /tmp/tracks_* 2>/dev/null", shell=True)  # noqa: S607, S602
-        return file_to_send
+        return safe_join("./transfer/", self.final_file_name)
 
 
 list_item_template = """<li class="mdc-list-item">
@@ -94,7 +97,7 @@ list_item_template = """<li class="mdc-list-item">
 
 # Generate the html elements for the previous files.
 def update_file_list():
-    path, dirs, files = next(walk("./downloads/"))
+    path, dirs, files = next(walk(storage_folder))
     prev_count = len(files)
     if prev_count == 0:
         return ["", ""]
@@ -112,25 +115,23 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/', methods=["GET", "POST"])
 def index_page():
     if request.method == "GET":
-        # with open("./templates/site.html", "r") as site:
-        #    return site.read()
         table_items, js_addition = update_file_list()
         return render_template("./site.html", previous_items=table_items, extra_js=js_addition)
 
     if request.method == "POST":
         dl_request = Video(request.form)
         dl_request.download()
-        file_bytes = dl_request.send_files()
-        return jsonify({
-            "file_name": dl_request.final_file_name,
-            "file_contents": file_bytes,
-            "format": f"audio/{dl_request.mime_type}"
-        })
+        return dl_request.send_files()
 
 
 @app.route('/transfer/<filepath>', methods=["GET"])
 def download_file(filepath):
-    return send_from_directory('./downloads/', filepath, as_attachment=True)
+    return send_from_directory(storage_folder, filepath, as_attachment=True)
+
+
+@app.route('/update_state', methods=["GET"])
+def update_file_state():
+    return update_file_list()[0]
 
 
 def main():
@@ -216,7 +217,7 @@ def main():
     # Delete the previous downloaded tracks
     if parser.is_present("-p"):
         print(" * Purging downloaded tracks.")
-        call("rm ./downloaded_tracks/* ./downloads/* 2>/dev/null", shell=True)  # noqa: S602, S607
+        call(f"rm {downloads_folder}/* {storage_folder}/* 2>/dev/null", shell=True)  # noqa: S602, S607
 
     # Install the apt dependencies.
     if parser.is_present("-i"):
@@ -225,6 +226,14 @@ def main():
 
     # Run the app
     if parser.is_present("-d"):
+
+        # Create required directories if not present.
+        if not path.isdir(storage_folder):
+            makedirs(storage_folder)
+
+        if not path.isdir(downloads_folder):
+            makedirs(downloads_folder)
+
         local_ip = check_output(("ip a | grep \"inet \" | grep -v \"127.0.0.1\" "  # noqa: S607
                                  "| awk -F ' ' {'print $2'} | cut -d \"/\" -f1"), shell=True)  # noqa: S602
         print(f" * My local ip address is: {local_ip.decode('utf-8').rstrip()}")
